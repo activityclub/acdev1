247e4c4f05f7c574fcdcbcc073cc4437e186fe80
Andrew Arrow <andrew@0x7a69.net>
2016-03-26 12:13:15 -0700

    tricky

--- elevator/elevator.go
+++ elevator/elevator.go
@@ -8,11 +8,14 @@ import (
 	"time"
 )
 
+var MAX_FLOORS int = 99
+
 type Vator struct {
 	Number       int
 	CurrentFloor int
 	NextFloor    int
-	Passengers   int
+	Buttons      []bool
+	Passengers   []*Passenger
 }
 
 type Passenger struct {
@@ -22,9 +25,11 @@ type Passenger struct {
 }
 
 type Floor struct {
-	Number   int
-	WantUp   bool
-	WantDown bool
+	Number     int
+	WantUp     bool
+	WantDown   bool
+	m          sync.Mutex
+	Passengers []*Passenger
 }
 
 func random(min, max int) int {
@@ -34,6 +39,7 @@ func random(min, max int) int {
 func NewFloor(number int) *Floor {
 	f := Floor{}
 	f.Number = number
+	f.Passengers = make([]*Passenger, 0)
 	return &f
 }
 
@@ -41,7 +47,7 @@ func NewPassenger(name string) *Passenger {
 	p := Passenger{}
 	p.Name = fake.FirstName() + " " + fake.LastName()
 	p.CurrentFloor = 1
-	p.DesiredFloor = random(1, 99)
+	p.DesiredFloor = random(1, MAX_FLOORS)
 
 	return &p
 }
@@ -50,7 +56,8 @@ func NewVator(number int) *Vator {
 	v := Vator{}
 	v.Number = number
 	v.CurrentFloor = 1
-	v.Passengers = 0
+	v.Buttons = make([]bool, MAX_FLOORS)
+	v.Passengers = make([]*Passenger, 0)
 
 	return &v
 }
@@ -73,6 +80,9 @@ func (self *Floor) upButton() {
 func passengerCreator(floor1 *Floor) {
 	for {
 		p := NewPassenger("")
+		floor1.m.Lock()
+		floor1.Passengers = append(floor1.Passengers, p)
+		floor1.m.Unlock()
 		fmt.Printf("New Passenger (%s) just walked in on Floor 1.\n", p.Name)
 		fmt.Printf("  They want to be on Floor %d \n", p.DesiredFloor)
 		floor1.upButton()
@@ -86,10 +96,10 @@ func Run() {
 
 	wg.Add(1)
 
-	fmt.Println("Create 99 Floors...")
+	fmt.Println("Create Floors...")
 
 	floors := make([]*Floor, 0)
-	for i := 1; i < 100; i++ {
+	for i := 1; i < MAX_FLOORS+1; i++ {
 		f := NewFloor(i)
 		floors = append(floors, f)
 	}
