ab1304a241cb1f17b9a9ced7b4f6b0e5de37e791
Andrew Arrow <andrew@0x7a69.net>
2016-03-25 22:42:23 -0700

    progress

--- elevator/elevator.go
+++ elevator/elevator.go
@@ -2,8 +2,45 @@ package elevator
 
 import (
 	"fmt"
+	"time"
 )
 
+type Vator struct {
+	Number       int
+	CurrentFloor int
+	NextFloor    int
+	Passengers   int
+}
+
+func NewVator(number int) *Vator {
+	v := Vator{}
+	v.Number = number
+	v.CurrentFloor = 1
+	v.Passengers = 0
+
+	return &v
+}
+
+func (self *Vator) run() {
+	for {
+		fmt.Println("Vator ", self.Number)
+		time.Sleep(time.Second * 5)
+	}
+}
+
 func Run() {
-	fmt.Println("run")
+	fmt.Println("Create 3 Elevators...")
+
+	i := 1
+	for {
+		if i > 3 {
+			break
+		}
+
+		v := NewVator(i)
+		go v.run()
+
+		i++
+
+	}
 }
